c068c7dcde4c869216d99ead24b49c3d98b7c756
Adrian Mejia <adriansky@gmail.com>
2013-08-01 09:38:06 -0400

    adds validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.where(user_id: current_user.id)
   end
 
   # GET /products/1
@@ -26,6 +26,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
+    @product.user = current_user
 
     respond_to do |format|
       if @product.save
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   belongs_to :user
+  validates :title, presence: true
 end
