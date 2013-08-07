8b2c90aadbff9e29c1654c532e62b41fe8abd87a
Adrian Mejia <adriansky@gmail.com>
2013-08-07 16:44:49 -0400

    tests users sign up

---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'User Sign Up' do
+  before do
+    visit root_path
+    click_link('Sign up')
+  end
+
+  context 'failure' do
+    before do
+      fill_in 'Email', with: ''
+      fill_in 'user[password]', with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_button 'Sign up'
+    end
+
+    it 'displays an error message' do
+      expect(page).to have_content('can\'t be blank')
+    end
+
+    it 'shows the correct navigation links' do
+      expect(page).to have_button('Sign up')
+    end    
+  end
+
+  context 'success' do
+    before do
+      fill_in 'Email', with: 'jill@example.com'
+      fill_in 'user[password]', with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_button 'Sign up'
+    end
+
+    it 'displays a welcome message' do
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+
+    it 'shows the correct navigation links' do
+      expect(page).to have_link('Logout')
+    end
+  end
+end
